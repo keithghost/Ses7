7a0a426991bab4cad993e80aa09ca6e6d0fd9c2c
keithghost <keithalphamd@gmail.com>
2025-06-18 08:58:21 +0300

    Update keith.js

--- keith.js
+++ keith.js
@@ -27,10 +27,7 @@ app.use('/', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`
-Bwm XMD scanner online ✅
-
-Made by Ibrahim Adams
-
+keith initialized successfully ✅
 Server running on http://localhost:${PORT}
     `);
 });
